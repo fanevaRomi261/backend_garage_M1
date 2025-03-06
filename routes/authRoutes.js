ce58e2dac686b941859430667a05b6ab7b4aeee2
fanevaRomi261 <118768031+fanevaRomi261@users.noreply.github.com>
2025-03-06 18:54:18 +0300

    correction structure

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,67 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Utilisateur = require('../models/Utilisateur');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const authController = require('../controllers/authController');
 
 // login
-router.post("/login", async (req, res) => {
-  const { mail, pwd } = req.body;
-
-  try {
-    const user = await Utilisateur.findOne({ mail });
-
-    if (!user) {
-      return res.status(400).json({ message: 'Utilisateur non trouvé' });
-    }
-
-    const isMatch = await bcrypt.compare(pwd, user.pwd);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Mot de passe incorrect' });
-    }
-
-    const token = jwt.sign({ userId: user._id }, 'secretkey', { expiresIn: '1h' });
-
-    res.json({
-      message: 'Connexion réussie',
-      token
-    });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Erreur serveur' });
-  }
-});
+router.post("/login",authController.login);
 
 // Inscription
-router.post("/register", async (req, res) => {
-    const { nom, prenom, mail, pwd, dtn, profil_id } = req.body;
-  
-    try {
-      const existingUser = await Utilisateur.findOne({ mail });
-      if (existingUser) {
-        return res.status(400).json({ message: "Cet email est déjà utilisé." });
-      }
-  
-      const saltRounds = 10;
-      const hashedPwd = await bcrypt.hash(pwd, saltRounds);
-  
-      const newUser = new Utilisateur({
-        nom,
-        prenom,
-        mail,
-        pwd: hashedPwd,
-        dtn,
-        profil_id
-      });
-  
-      await newUser.save();
-  
-      res.status(201).json({ message: "Inscription réussie !" });
-    } catch (error) {
-      console.error("Erreur lors de l'inscription :", error);
-      res.status(500).json({ message: "Erreur serveur" });
-    }
-  });
+router.post("/register",authController.register);
 
 module.exports = router;
