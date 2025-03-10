9be5a47c5465f79a354d2de04e2e626f082eadc9
fanevaRomi261 <118768031+fanevaRomi261@users.noreply.github.com>
2025-03-10 22:30:31 +0300

    ajout vehicule

--- routes/vehiculeRoutes.js
+++ routes/vehiculeRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const vehiculeController = require("../controllers/vehiculeController");
 const { verifToken, verifProfil } = require('../middlewares/authMiddleware');
+const { validateAjoutVehicule, validateUpdateVehicule } = require("../validators/vehiculeValidator");
 
-router.post("/", verifToken,verifProfil(["Client"]),vehiculeController.ajouterVehicule);
+router.post("/", verifToken,verifProfil(["Client"]),validateAjoutVehicule,vehiculeController.ajouterVehicule);
 
 // verifier si connecté slm car tous les profils pourront voir la liste des vehicules d'une personne donnée
 router.get("/utilisateur/:id", verifToken,vehiculeController.getVehiculesParUtilisateur);
 
-router.put("/utilisateur/:id", verifToken,verifProfil(["Client"]),vehiculeController.updateVehicule);
+router.put("/:id", verifToken,verifProfil(["Client"]),validateAjoutVehicule,vehiculeController.updateVehicule);
 
 module.exports = router;
