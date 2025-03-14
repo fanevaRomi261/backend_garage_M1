6bcf47f5163237acd0a69f802de62f3baee96944
Ismael-Raveloson <isma.raveloson@gmail.com>
2025-03-14 13:58:23 +0300

    test

--- controllers/planningController.js
+++ controllers/planningController.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const RendezVous = require("../models/RendezVous");
+const Utilisateur = require("../models/Utilisateur");
+const Profil = require("../models/Profil");
 
-const findMecanicienLibre = (date) =>{
-
+const findMecanicienLibre = async(date) =>{
+    const profil = await Profil.findOne({libelle: 'Mécanicien'});
+    const mecaniciens = await Utilisateur.find({profil_id: profil._id});
+    for(meca in mecaniciens){
+        console.log(meca.nom);
+    }
 };
 
 const proposeHeureRendezVous = (date) =>{
